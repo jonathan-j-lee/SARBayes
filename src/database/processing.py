f66785523a76cf418d19281f255d8db683db78f5
jonathanlee1 <jonalee22@gmail.com>
2016-05-10 13:03:02 -0400

    Refactoring queryable properties

--- src/database/processing.py
+++ src/database/processing.py
@@ -3,7 +3,7 @@ database.processing
 ===================
 """
 
-__all__ = ['survival_rate']
+__all__ = ['survival_rate', 'tabulate']
 
 import numpy as np
 import pandas as pd
@@ -14,3 +14,7 @@ from .models import Base, Subject
 def survival_rate(subjects):
     subjects = subjects.filter(Subject.status != None)
     return subjects.filter(Subject.survived).count()/subjects.count()
+
+
+def tabulate(*columns):
+    ...
