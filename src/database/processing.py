79b34b9a9405dace583f9174f4b8ddd9c4cd1a93
jonathanlee1 <jonalee22@gmail.com>
2016-05-29 21:04:17 -0400

    Refactoring, new slides

--- src/database/processing.py
+++ src/database/processing.py
@@ -7,8 +7,9 @@ __all__ = ['survival_rate', 'tabulate']
 
 import numpy as np
 import pandas as pd
+import re
 
-from .models import Base, Subject
+from .models import Subject
 
 
 def survival_rate(subjects):
@@ -16,5 +17,27 @@ def survival_rate(subjects):
     return subjects.filter(Subject.survived).count()/subjects.count()
 
 
-def tabulate(*columns):
-    ...
+def tabulate(query, not_null=True):
+    columns = query.column_descriptions
+
+    if not_null:
+        criteria = map(lambda column: column['expr'] != None, columns)
+        query = query.filter(*criteria)
+
+    df = pd.read_sql(query.statement, query.session.bind)
+    df.columns = list(map(lambda column: column['name'], columns))
+
+    return df
+
+
+def standardize_category(category, merge=True):
+    category = category.strip().casefold()
+
+    if merge:
+        for keyword in 'child', 'skier', 'vehicle':
+            pattern = r'\b({})\b'.format(keyword)
+            result = re.search(pattern, category)
+            if result:
+                return result.group(1)
+
+    return category
