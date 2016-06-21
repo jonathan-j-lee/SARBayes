6b2c2cdd017c906f650291db41909637f24d75a9
jonathanlee1 <jonalee22@gmail.com>
2016-06-21 17:14:50 -0400

    Finished UI

--- src/server/main.py
+++ src/server/main.py
@@ -7,7 +7,7 @@ $ bokeh serve --show server
 from bokeh.models import Button, CheckboxButtonGroup, MultiSelect, Panel, Slider, Tabs, HBox, VBox
 from bokeh.plotting import figure, curdoc, vplot
 from collections import Counter
-from functools import reduce
+from lifelines import KaplanMeierFitter
 
 import database
 from database.models import Incident, Group, Subject
@@ -19,7 +19,7 @@ from database.processing import tabulate
 engine, session = database.initialize('sqlite:///../data/isrid-master.db')
 
 query = session.query(Subject.survived, Incident.total_hours, Group.category,
-                      Group.size, Subject.age, Subject.sex_as_str)
+                      Group.size, Subject.age, Subject.sex)
 query = query.join(Group, Incident)
 df = tabulate(query)
 
@@ -32,21 +32,25 @@ plot = figure()
 
 categories = Counter(df['category'])
 categories = sorted(categories, key=lambda category: -categories[category])
-
 category_select = MultiSelect(value=categories, options=categories,
                               title='Category')
-size_select = Slider(start=1, end=max(df['size']), value=1, step=1,
-                     title='Group Size')
-age_min_select = Slider(start=0, end=max(df['age']), value=0, step=1,
+
+min_size_select = Slider(start=1, end=max(df['size']), value=1, step=1,
+                         title='Minimum Group Size')
+max_size_select = Slider(start=1, end=max(df['size']), value=max(df['size']),
+                         step=1, title='Maximum Group Size')
+
+min_age_select = Slider(start=0, end=max(df['age']), value=0, step=1,
                         title='Minimum Age')
-age_max_select = Slider(start=0, end=max(df['age']), value=max(df['age']),
+max_age_select = Slider(start=0, end=max(df['age']), value=max(df['age']),
                         step=1, title='Maximum Age')
+
 sex_select = CheckboxButtonGroup(labels=['Male', 'Female'], active=[0, 1])
 
 selectors = Tabs(tabs=[
     Panel(child=HBox(category_select), title='Category'),
-    Panel(child=HBox(size_select), title='Group Size'),
-    Panel(child=HBox(age_min_select, age_max_select), title='Age'),
+    Panel(child=HBox(min_size_select, max_size_select), title='Group Size'),
+    Panel(child=HBox(min_age_select, max_age_select), title='Age'),
     Panel(child=sex_select, title='Sex')
 ])
 
@@ -54,14 +58,29 @@ def callback():
     """
     Dynamically generate a Kaplan-Meier plot.
     """
-    """
-    df_ = df[age_min_select.value <= df.age]
-    df_ = df_[df_.age <= age_max_select.value]
+    df_ = df.copy()
+
+    print(category_select.value)
+    for category in categories:
+        if category not in category_select.value:
+            df_ = df_[df_.category != category]
+
+    df_ = df_[min_size_select.value <= df_['size']]
+    df_ = df_[df_['size'] <= max_size_select.value]
+
+    df_ = df_[min_age_select.value <= df_.age]
+    df_ = df_[df_.age <= max_age_select.value]
+
     if 0 not in sex_select.active:
-        df_ = df_[df_.sex_as_str != 'male']
+        df_ = df_[df_.sex != 1]
     if 1 not in sex_select.active:
-        df_ = df_
-    """
+        df_ = df_[df_.sex != 2]
+
+    days = [total_hours.total_seconds()/3600/24
+            for total_hours in df.total_hours]
+
+    kmf = KaplanMeierFitter()
+    #kmf.fit()
 
 callback()
 
