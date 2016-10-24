b9383962b37aa39acd7302919845ccdabd3601df
Jonathan Lee <jonathan-lee@berkeley.edu>
2016-10-23 20:21:36 -0700

    Documented database processing, moved old survival curves figure

--- src/database/processing.py
+++ src/database/processing.py
@@ -1,6 +1,5 @@
 """
-database.processing
-===================
+database.processing -- Database processing tools
 """
 
 __all__ = ['survival_rate', 'tabulate', 'export_to_orange']
@@ -9,15 +8,43 @@ import numpy as np
 from Orange.data import ContinuousVariable, DiscreteVariable, Domain, Table
 import pandas as pd
 
-from .models import Subject
+from database.models import Subject
 
 
 def survival_rate(subjects):
+    """
+    Calculate the survival rate of a group of subjects.
+
+    Arguments:
+        subjects: An SQLAlchemy `Query` object of `Subject` instances.
+
+    Returns:
+        A float between zero and one, inclusive, representing the survival rate
+        of the given subjects with a definite status.
+    """
     subjects = subjects.filter(Subject.status != None)
     return subjects.filter(Subject.survived).count()/subjects.count()
 
 
 def tabulate(query, not_null=True):
+    """
+    Convert an SQLAlchemy `Query` object into a Pandas dataframe.
+
+    Arguments:
+        query: An `SQLAlchemy` query object.
+        not_null: A list of booleans, one per column in the query. For each
+                  column, if its corresponding value in `not_null` is `True`,
+                  exclude all rows where that column's value is null.
+                  Alternatively, a single boolean can be supplied, which is
+                  applied to all columns.
+
+    Returns:
+        A Pandas dataframe containing the rows in the query.
+
+    Raises:
+        ValueError: if `not_null` is a list and its size is not the same as the
+                    number of columns in the query.
+    """
     columns = query.column_descriptions
 
     if isinstance(not_null, bool):
@@ -36,7 +63,22 @@ def tabulate(query, not_null=True):
 
 
 def export_to_orange(df, *class_names,
-                  from_duration=lambda delta: delta.total_seconds()/3600):
+                     from_duration=lambda delta: delta.total_seconds()/3600):
+    """
+    Convert a Pandas dataframe to an Orange `Table`.
+
+    Arguments:
+        df: A Pandas dataframe.
+        class_names: The columns in the dataframe considered to be class
+                     variables.
+        from_duration: A one-argument function that converts a
+                       `numpy.timedelta64` object into a number. By default,
+                       the number of seconds is used.
+
+    Returns:
+        An appropriately configured Orange `Table` object containing the data
+        in `df`.
+    """
     features, classes = [], []
 
     for name, dtype in zip(df.columns, df.dtypes):
